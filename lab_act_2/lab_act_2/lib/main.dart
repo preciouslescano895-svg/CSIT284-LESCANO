59dcf2a784d623b48e497556b39449e8bc72bd21
LAB12_USER ON WS16 <L12X16W16@LabHCI.net>
2026-08-19 17:48:44 +0800

    Center hello world

--- lab_act_2/lab_act_2/lib/main.dart
+++ lab_act_2/lab_act_2/lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main () {
-  runApp(MaterialApp(home: Text("Hello World")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("Hello World"),
+            ),
+          ),
+        ),
+      ),
+    );
 }

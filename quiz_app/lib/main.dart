9f1ec6fdc8cb79bb8c90b6a497b42fc661db282e
Precious Lescano <preciouslescano895@gmail.com>
2026-09-13 15:15:39 +0800

    Design Flutter interface

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'package:basics/gradient_container.dart';
+import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 33, 5, 109),
-          Color.fromARGB(255, 68, 21, 149),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
         ),
       ),
     ),
